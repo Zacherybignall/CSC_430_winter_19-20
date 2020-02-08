02b566be2e3cdb4932bccdafa023e78880cd52be
Zacherybignall <Zacherybignall@gmail.com>
2020-02-08 13:50:43 -0600

    tables all updated and "loaded" in
    no idea if the tables will work but all are in the sql file , fix one or two .
    also bu tthe good ER digram in here so we can all goo of that .
    next should be to fill in teh tables

--- db_test.sql
+++ db_test.sql
@@ -1,7 +1,11 @@
--- Database: "R1_test"
---
--- -----------------------
 
+-- Database: "R1_test"
+/*
+by :
+Anthony Toussaint
+Tracy Samanie
+Zachery Bignall 
+*/
 --
 CREATE DATABASE R1_TEST;
 -- DROP DATABASE R1_TEST;
@@ -22,7 +26,7 @@ create table Location(
 	l_addr varchar(50) DEFAULT NULL, 
 	l_phnum int(10) NOT NULL, 
 	l_name varchar(25) NOT NULL, 
-	income DECIMAL(19,9) NOT NULL,
+	Income DECIMAL(19,9) NOT NULL,
     PRIMARY key (l_id)
 );
 
@@ -34,7 +38,8 @@ create table Works_for(
     Constraint l_idFK foreign key (l_id) references Location(l_id),
     Constraint empl_idFK foreign key (empl_id) references Employee(empl_id)
     );
-    
+# leave out mangers and supervises for now , need to underatand how supervies would work frist 
+/*    
 create table Manages(
 	l_id int(11) NOT NULL,
     empl_id int(11) NOT NULL,
@@ -48,19 +53,76 @@ create table Manages(
 	empl_id int(11) NOT NULL,
     supevisor varchar(25) NOT NULL, -- gotta make this a composite/ multi valued?
     supervisee varchar(25) NOT NULL, -- gotta make this a composite/ multi valued?
-    primary key (empl_id), */ 
+    primary key (empl_id),
+    */
     
 
 -- still a bit confused on how to do these below
 create table Buyer(
-date varchar(20) NOT NULL, -- composite/multi value
-car_info varchar(50) NOT NULL, -- composite/multi value
-b_name varchar(25) NOT NULL -- composite 
--- no prime key atm 
+	b_name varchar(25) NOT NULL, -- composite prime key , leave as varchar
+    b_id INT NOT NULL,
+	date varchar(20) , -- composite/multi value, leave as varchar
+	car_info varchar(50), -- composite/multi value, leave as varchar 
+	 primary key(b_id)
 );
-
- 
-/* create table Department(
-	 new_inv varchar()
-     used_inv varchar()
- */
+-- from loction , buys_form
+create table Buy_form(
+	invoice_num INT,
+	L_id INT NOT NULL,
+	b_id varchar(40) NOT NULL,	
+    Constraint L_id foreign key (L_id) references Location(L_id),
+    Constraint b_id foreign key (b_id) references Buyer(b_id),
+    primary key(l_id,b_id)
+	);
+ -- Department 
+create table Department(
+	 Dep_id INT NOT NULL,
+     d_name varchar(25),
+     d_salary INT,
+     primary key (Dep_id)
+);
+-- from Department , sells , has , repaiers
+create table Sells(
+	Dep_id INT NOT NULL,
+	VIN_NUM varchar(40) NOT NULL,	
+    Constraint Dep_id foreign key (Dep_id) references Department(Dep_id),
+    Constraint VIN_NUM foreign key (VIN_NUM) references Inventory(VIN_NUM),
+    primary key(Dep_id,VIN_NUM)
+	);
+create table Has(
+	Dep_id INT NOT NULL,
+	Part_num varchar(40) NOT NULL,	
+    Constraint Dep_id foreign key (Dep_id) references Department(Dep_id),
+    Constraint Part_num foreign key (Part_num) references Parts(Part_num),
+    primary key(Dep_id,Part_num)
+	);
+    create table Repairs(
+	Dep_id INT NOT NULL,
+	Repair_num INT NOT NULL,	
+    Constraint Dep_id foreign key (Dep_id) references Department(Dep_id),
+    Constraint Repair_num foreign key (Repair_num) references Car(Repair_num),
+    primary key(Dep_id,Repair_num)
+	);
+    -- parts
+    create table Parts(
+    part_name varchar(35), 
+	part_num INT NOT NULL, 
+	part_price INT NOT NULL, 
+    PRIMARY key (part_num)
+    );
+    -- car 
+   create table Car(
+   repair_num INT NOT NULL,
+   VIN_NUM varchar(50),
+   repair_price INT NOT NULL,
+   PRIMARY key (repair_num)
+   );
+   -- inventory 
+   create table Inventory(
+   new_used int(1),
+   car_num INT,
+   company varchar(15),
+   VIN_NUM varchar(35) NOT NULL,
+   car_price INT NOT NULL,
+   PRIMARY key (VIN_NUM)
+   );
