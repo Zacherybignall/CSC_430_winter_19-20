53e57c89e9ff93ac277d9de163bd524183f34294
tas054 <38105302+tas054@users.noreply.github.com>
2020-02-26 21:45:11 -0600

    Update LOAN.sql

--- LOAN.sql
+++ LOAN.sql
@@ -18,7 +18,7 @@ create table Employee(
 	e_salary DECIMAL(19,9) NOT NULL,
 	e_name varchar(25) NOT NULL, -- gotta make this a composite/ multi valued?
 	PRIMARY KEY (empl_id)
-);
+	);
 
 create table Customer(
 	customer_id int NOT NULL AUTO_INCREMENT,
@@ -26,39 +26,39 @@ create table Customer(
 	c_phone int  NOT NULL,
 	c_name varchar(25) NOT NULL, -- gotta make this a composite/ multi valued?
 	PRIMARY KEY (customer_id)
-);
+	);
 
 #can add nmilage, date aquired
 create table Cars(
 	vin_num varchar(45) not null, 
 	new_used enum ('new','used'),
 	make varchar(45),
-    model varchar(45),
-    suggest_price INT NOT NULL,
+    	model varchar(45),
+    	suggest_price INT NOT NULL,
 	PRIMARY key (VIN_NUM)
-   );
+   	);
    
 create table Car_sold(
 	car_sold_id int not null, 
 	vin_num varchar(45),
-    customer_id int,
-    agreed_price int,
-    date_sold date,
-    empl_id int,
-    empl_comission int,
-    primary key (car_sold_id),
+    	customer_id int,
+    	agreed_price int,
+    	date_sold date,
+    	empl_id int,
+    	empl_comission int,
+    	primary key (car_sold_id),
 	Constraint vin_numFK foreign key (vin_num) references Cars(vin_num),
-    Constraint customer_idFK foreign key (customer_id) references Customer(customer_id),
-    Constraint empl_idFK foreign key (empl_id) references Employee(empl_id)
-    );
+    	Constraint customer_idFK foreign key (customer_id) references Customer(customer_id),
+    	Constraint empl_idFK foreign key (empl_id) references Employee(empl_id)
+    	);
     
 create table car_loan(
 	loan_id int not null,
 	car_sold_id int,
 	loan_price int, 
 	primary key (loan_id),
-    constraint loan_idFK2 foreign key (car_sold_id) references Car_sold(car_sold_id)
-    );
+    	constraint loan_idFK2 foreign key (car_sold_id) references Car_sold(car_sold_id)
+    	);
     
 
 insert into Employee (empl_id, e_addr, e_name, e_salary) values 
@@ -87,9 +87,10 @@ select c.empl_id, c.vin_num, c.customer_id
 from car_sold c, (select loan_price,car_sold_id from car_loan) cr, (select suggest_price,vin_num from cars) ca
 where cr.car_sold_id = c.car_sold_id and c.vin_num = ca.vin_num and ((c.agreed_price < (cr.loan_price*1.5)) or ((ca.suggest_price) < (cr.loan_price*1.2)) or (c.empl_comission) >= (cr.loan_price*0.02));
 
-#veiw does not see anything  wrong
+#veiw does not see anything wrong
 create view	cars_sold as
 	select c.car_sold_id,c.vin_num,ca.new_used,c.agreed_price
-    from car_sold c,cars ca
-    where ca.vin_num = c.vin_num;
+    	from car_sold c,cars ca
+    	where ca.vin_num = c.vin_num;
+	
 SELECT * FROM cars_sold;
